6cd87503b1b0a8c94e342b85acb99d8fbc3b228a
Christopher Rodriguez <5028527+mrmcpowned@users.noreply.github.com>
2018-02-11 15:11:53 +0000

    Oh god please why

--- client.py
+++ client.py
@@ -1,10 +1,37 @@
 import requests, json
 import serial
+import time
+from gpio_96boards import GPIO
 
+LEFT = GPIO.gpio_id('GPIO_B')
+RIGHT = GPIO.gpio_id('GPIO_A')
+
+pins = (
+    (LEFT, 'out'),
+    (RIGHT, 'out')
+)
+
+
+def blink(pin):
+    with GPIO(pins) as gpio:
+        for i in range(3):
+            gpio.digital_write(pin[0], GPIO.HIGH)
+            time.sleep(i)
+            gpio.digital_write(pin[0], GPIO.LOW)
+            time.sleep(1)
 
 
 while True:
-    signals = json.loads(requests.get('http://127.0.0.1:5000/').text)
-    if signals['right']:
-        
-    elif signals['left']:
+    try:
+        signals = json.loads(requests.get('http://983f4cb9.ngrok.io').text)
+        if signals['right']:
+            blink(pins[1])
+            reqeusts.get('http://983f4cb9.ngrok.io/right')
+        elif signals['left']:
+            reqeusts.get('http://983f4cb9.ngrok.io/left')
+            blink(pins[0])
+
+        time.sleep(1)
+    except:
+       print "Rip JSON"
+
